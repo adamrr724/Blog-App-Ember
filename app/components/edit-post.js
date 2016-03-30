de4f0c050755cf4a85b065656685477084132be7
Alex Fallenstedt and adam ross russell <a.fallenstedt@gmail.com>
2016-03-30 10:44:03 -0700

    add functionality to update a post

--- app/components/edit-post.js
+++ app/components/edit-post.js
@@ -7,7 +7,17 @@ export default Ember.Component.extend({
     editPost() {
       this.set('editPost', true);
     },
-    save
+    savePost(post) {
+      var params = {
+        title: this.get('title'),
+        author: this.get('author'),
+        date: this.get('date'),
+        body: this.get('body'),
+        image: this.get('image')
+      };
+      this.sendAction('savePost2', post, params);
+      this.set('editPost', false);
+    }
   }
 
 
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -12,10 +12,19 @@ export default Ember.Route.extend({
       this.transitionTo('admin');
     },
     delete(post) {
-      if(confirm("Are you sure you would like to delete '{{post.title}}?'")) {
+      if(confirm("Are you sure you would like to delete this post?")) {
         post.destroyRecord();
         this.transitionTo('admin');
       }
+    },
+    savePost3(post, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key] !== undefined) {
+          post.set(key, params[key]);
+        }
+      });
+      post.save();
+      this.transitionTo('admin');
     }
   }
 
