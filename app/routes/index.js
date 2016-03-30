992df2d83302a1957c555db63ae0c510c483176d
Alex Fallenstedt and adam ross russell <a.fallenstedt@gmail.com>
2016-03-30 09:09:11 -0700

    add edit-post component

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(){
-    return this.store.findAll('posts');
+    return this.store.findAll('post');
   }//eHre, we specify this.store to refer to the Firebase data store we've set up for our application. Then the findAll method with the argument rental instructs Ember Data to find all records of the type posts in the store, and return them to our application.
 });
