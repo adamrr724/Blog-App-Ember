704c372da85d141293c9428588c65a7495f0a9e2
Alex Fallenstedt and adam ross russell <adamrr724@yahoo.com>
2016-03-30 09:49:15 -0700

    have functional form to add new posts

--- app/components/new-post.js
+++ app/components/new-post.js
@@ -5,15 +5,15 @@ export default Ember.Component.extend({
   actions: {
     showNewPostForm(){
       this.set('addNewPost', true);
-    }
+    },
     addNewPost(){
-      var params {
+      var params = {
         title: this.get('title'),
         author: this.get('author'),
         date: this.get('date'),
         body: this.get('body'),
         image: this.get('image')
-      }
+      };
       this.sendAction('addNewPost2', params);
     }
   }
