29904d91a1ee6f8a1b5f796e42ab495202ba0da9
alex fallenstedt and adam ross russell <adamrr724@yahoo.com>
2016-03-31 10:06:22 -0700

    IT ADDS COMMENTS HALELUJAH

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(){
-    return this.store.findAll('post');
+    return  Ember.RSVP.hash({
+      post: this.store.findAll('post'),
+      comment: this.store.findAll('comment')
+    });
+
   }//eHre, we specify this.store to refer to the Firebase data store we've set up for our application. Then the findAll method with the argument rental instructs Ember Data to find all records of the type posts in the store, and return them to our application.
 });
