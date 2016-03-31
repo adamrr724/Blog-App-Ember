10d859e2ef7bf989aa6ed6db66eacb4182dd81e5
alex fallenstedt and adam ross russell <adamrr724@yahoo.com>
2016-03-31 10:02:01 -0700

    pass addNewComment1-3 to post.js

--- app/components/new-comment.js
+++ app/components/new-comment.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  actions: {
+    addNewComment() {
+      var params = {
+        name: this.get('name'),
+        rating: this.get('rating'),
+        body: this.get('body'),
+        post: this.get('post')
+      };
+      this.sendAction('addNewComment2', params);
+    }
+  }
 });
